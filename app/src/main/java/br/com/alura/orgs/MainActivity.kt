6766069707ca2608e1661054347be3fef58abee6
André Akira <38569716+Akiira23@users.noreply.github.com>
2022-05-11 14:48:06 -0300

    Implementado layout do produto e buscando views do layout na Activity

--- app/src/main/java/br/com/alura/orgs/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/MainActivity.kt
@@ -8,5 +8,12 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val nome = findViewById<TextView>(R.id.nome)
+        nome.text = "Cesta de Frutas"
+        val descricao = findViewById<TextView>(R.id.descricao)
+        descricao.text = "Laranja, manga e maçã"
+        val valor = findViewById<TextView>(R.id.valor)
+        valor.text = "R$ 12,00"
+
     }
 }
