cca6484a7de31d1258d377809f3b7cfaa6a7f125
André Akira <38569716+Akiira23@users.noreply.github.com>
2022-05-11 16:41:03 -0300

    Recursos do AS e configuração de build

--- app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.alura.orgs.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.orgs.R
@@ -10,16 +11,9 @@ import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 import java.math.BigDecimal
 import java.sql.DriverManager.println
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-//        val nome = findViewById<TextView>(R.id.nome)
-//        nome.text = "Cesta de Frutas"
-//        val descricao = findViewById<TextView>(R.id.descricao)
-//        descricao.text = "Laranja, manga e maçã"
-//        val valor = findViewById<TextView>(R.id.valor)
-//        valor.text = "R$ 12,00"
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = ListaProdutosAdapter(
             context = this, produtos = listOf(
