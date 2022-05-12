a6051cf0b552f2fa07e3fc7add8eb34eb2d00b70
André Akira <38569716+Akiira23@users.noreply.github.com>
2022-05-12 10:16:11 -0300

    Implementado ViewBinding no projeto

--- app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -1,22 +1,26 @@
 package br.com.alura.orgs.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import br.com.alura.orgs.R
+import androidx.appcompat.app.AppCompatActivity
 import br.com.alura.orgs.dao.ProdutosDao
+import br.com.alura.orgs.databinding.ActivityFormularioProdutoBinding
 import br.com.alura.orgs.model.Produto
 import java.math.BigDecimal
 
-class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
+class FormularioProdutoActivity : AppCompatActivity() {
+
+    private val biding by lazy {
+        ActivityFormularioProdutoBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         configuraBotaoSalvar()
+        setContentView(biding.root)
     }
 
     private fun configuraBotaoSalvar() {
-        val botaoSalvar = findViewById<Button>(R.id.produto_item_botaoSalvar)
+        val botaoSalvar = biding.produtoItemBotaoSalvar
         val dao = ProdutosDao()
 
         botaoSalvar.setOnClickListener {
@@ -26,13 +30,13 @@ class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario
     }
 
     private fun criaProduto(): Produto {
-        val campoNome = findViewById<EditText>(R.id.activity_formulario_nome)
+        val campoNome = biding.activityFormularioNome
         val nome = campoNome.text.toString()
 
-        val campoDescricao = findViewById<EditText>(R.id.activity_formulario_descricao)
+        val campoDescricao = biding.activityFormularioDescricao
         val descricao = campoDescricao.text.toString()
 
-        val campoValor = findViewById<EditText>(R.id.activity_formulario_valor)
+        val campoValor = biding.activityFormularioValor
         val valorEmTexto = campoValor.text.toString()
         val valor = if (valorEmTexto.isBlank()) {
             BigDecimal.ZERO
--- app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActvity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActvity.kt
@@ -3,20 +3,29 @@ package br.com.alura.orgs.ui.activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
-import br.com.alura.orgs.R
 import br.com.alura.orgs.dao.ProdutosDao
+import br.com.alura.orgs.databinding.ListaProdutosActivityBinding
 import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class ListaProdutosActvity : AppCompatActivity(R.layout.activity_main) {
+class ListaProdutosActvity : AppCompatActivity() {
 
     private val dao = ProdutosDao()
     private val adapter = ListaProdutosAdapter(context = this, produtos = dao.buscaTodos())
+    private val binding by lazy {
+        ListaProdutosActivityBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         configuraRecyclerView()
         configuraFab()
+        setContentView(binding.root)
+    }
+
+    private fun configuraRecyclerView() {
+        val recyclerView = binding.activityListaRecyclerView
+        recyclerView.adapter = adapter
     }
 
     override fun onResume() {
@@ -25,7 +34,7 @@ class ListaProdutosActvity : AppCompatActivity(R.layout.activity_main) {
     }
 
     private fun configuraFab() {
-        val fab = findViewById<FloatingActionButton>(R.id.activity_lista_floatingActionButton)
+        val fab = binding.activityListaFloatingActionButton
         fab.setOnClickListener {
             vaiParaFormularioProduto()
         }
@@ -34,10 +43,5 @@ class ListaProdutosActvity : AppCompatActivity(R.layout.activity_main) {
     private fun vaiParaFormularioProduto() {
         val intent = Intent(this, FormularioProdutoActivity::class.java)
         startActivity(intent)
-    }
-
-    private fun configuraRecyclerView() {
-        val recyclerView = findViewById<RecyclerView>(R.id.activity_lista_recyclerView)
-        recyclerView.adapter = adapter
     }
 }
