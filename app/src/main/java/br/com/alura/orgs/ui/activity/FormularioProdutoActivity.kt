947cf3560ef947922253e0c46f7ff23f1e9a0a7f
André Akira <38569716+Akiira23@users.noreply.github.com>
2022-05-12 12:06:08 -0300

    Melhorando o visual do formulário
    Aula 03

--- app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -1,7 +1,9 @@
 package br.com.alura.orgs.ui.activity
 
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import br.com.alura.orgs.R
 import br.com.alura.orgs.dao.ProdutosDao
 import br.com.alura.orgs.databinding.ActivityFormularioProdutoBinding
 import br.com.alura.orgs.model.Produto
@@ -18,10 +20,21 @@ class FormularioProdutoActivity : AppCompatActivity() {
 
         setContentView(binding.root)
         configuraBotaoSalvar()
+        binding.activityFormularioProdutoImagem.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setView(R.layout.formulario_imagem)
+                .setPositiveButton("Confirmar") { _, _ ->
+
+                }
+                .setNegativeButton("Cancelar") { _, _ ->
+
+                }
+                .show()
+        }
     }
 
     private fun configuraBotaoSalvar() {
-        val botaoSalvar = binding.produtoItemBotaoSalvar
+        val botaoSalvar = binding.activityFormularioProdutoBotaoSalvar
         val dao = ProdutosDao()
 
         botaoSalvar.setOnClickListener {
@@ -31,13 +44,13 @@ class FormularioProdutoActivity : AppCompatActivity() {
     }
 
     private fun criaProduto(): Produto {
-        val campoNome = binding.activityFormularioNome
+        val campoNome = binding.activityFormularioProdutoNome
         val nome = campoNome.text.toString()
 
-        val campoDescricao = binding.activityFormularioDescricao
+        val campoDescricao = binding.activityFormularioProdutoDescricao
         val descricao = campoDescricao.text.toString()
 
-        val campoValor = binding.activityFormularioValor
+        val campoValor = binding.activityFormularioProdutoValor
         val valorEmTexto = campoValor.text.toString()
         val valor = if (valorEmTexto.isBlank()) {
             BigDecimal.ZERO
