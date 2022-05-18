a48aa40992c829eece265a3dd12d11e97529e707
André Akira <38569716+Akiira23@users.noreply.github.com>
2022-05-18 13:24:25 -0300

    master

--- app/src/main/java/br/com/alura/orgs/database/dao/ProdutoDao.kt
+++ app/src/main/java/br/com/alura/orgs/database/dao/ProdutoDao.kt
@@ -16,27 +16,5 @@ interface ProdutoDao {
     suspend fun remove(produto: Produto)
 
     @Query("SELECT * FROM Produto WHERE id = :id")
-<<<<<<< HEAD
-    fun buscaPorId(id: Long): Produto?
-
-    @Query("SELECT * FROM Produto ORDER BY nome DESC")
-    fun ordenaNomeDesc(): List<Produto>
-
-    @Query("SELECT * FROM Produto ORDER BY nome ASC")
-    fun ordenaNomeAsc(): List<Produto>
-
-    @Query("SELECT * FROM Produto ORDER BY descricao DESC")
-    fun ordenaDescricaoDesc(): List<Produto>
-
-    @Query("SELECT * FROM Produto ORDER BY descricao ASC")
-    fun ordenaDescricaoAsc(): List<Produto>
-
-    @Query("SELECT * FROM Produto ORDER BY valor DESC")
-    fun ordenaValorDesc(): List<Produto>
-
-    @Query("SELECT * FROM Produto ORDER BY valor ASC")
-    fun ordenaValorAsc(): List<Produto>
-=======
     fun buscaPorId(id: Long): Flow<Produto?>
->>>>>>> 10dbbacba3d14f74de98f9649d53f8af86cdf6c1
 }
--- app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActvity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActvity.kt
@@ -2,12 +2,7 @@ package br.com.alura.orgs.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
-<<<<<<< HEAD
-import android.view.*
-import android.widget.PopupMenu
-=======
 import android.util.Log
->>>>>>> 10dbbacba3d14f74de98f9649d53f8af86cdf6c1
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
@@ -16,16 +11,12 @@ import br.com.alura.orgs.database.AppDatabase
 import br.com.alura.orgs.databinding.ListaProdutosActivityBinding
 import br.com.alura.orgs.model.Produto
 import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
-<<<<<<< HEAD
-import br.com.alura.orgs.R
-=======
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import java.lang.Exception
->>>>>>> 10dbbacba3d14f74de98f9649d53f8af86cdf6c1
 
 class ListaProdutosActvity : AppCompatActivity() {
 
@@ -33,14 +24,9 @@ class ListaProdutosActvity : AppCompatActivity() {
     private val binding by lazy {
         ListaProdutosActivityBinding.inflate(layoutInflater)
     }
-<<<<<<< HEAD
-    private val produtoDao by lazy {
-        AppDatabase.instancia(this).produtoDao()
-=======
     private val dao by lazy {
         val db = AppDatabase.instancia(this)
         db.produtoDao()
->>>>>>> 10dbbacba3d14f74de98f9649d53f8af86cdf6c1
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,7 +35,6 @@ class ListaProdutosActvity : AppCompatActivity() {
         setContentView(binding.root)
         configuraRecyclerView()
         configuraFab()
-        
 
         val handler = coroutineExceptionHandler()
 
@@ -72,40 +57,12 @@ class ListaProdutosActvity : AppCompatActivity() {
         return handler
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_ordena_produtos, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val db = AppDatabase.instancia(this)
-        val produtoDao = db.produtoDao()
-        when (item.itemId) {
-            R.id.menu_ordena_produtos_nome_desc ->
-                adapter.atualiza(produtoDao.ordenaNomeDesc())
-            R.id.menu_ordena_produtos_nome_asc ->
-                adapter.atualiza(produtoDao.ordenaNomeAsc())
-            R.id.menu_ordena_produtos_descricao_desc ->
-                adapter.atualiza(produtoDao.ordenaDescricaoDesc())
-            R.id.menu_ordena_produtos_descricao_asc ->
-                adapter.atualiza(produtoDao.ordenaDescricaoAsc())
-            R.id.menu_ordena_produtos_valor_desc ->
-                adapter.atualiza(produtoDao.ordenaValorDesc())
-            R.id.menu_ordena_produtos_valor_asc ->
-                adapter.atualiza(produtoDao.ordenaValorAsc())
-            R.id.menu_ordena_produtos_sem_ordem ->
-                adapter.atualiza(produtoDao.buscaTodos())
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
     private fun configuraFab() {
         val fab = binding.activityListaFloatingActionButton
         fab.setOnClickListener {
             vaiParaFormularioProduto()
         }
     }
-
 
     private fun configuraRecyclerView() {
         val recyclerView = binding.activityListaRecyclerView
@@ -118,28 +75,6 @@ class ListaProdutosActvity : AppCompatActivity() {
                 putExtra(CHAVE_PRODUTO_ID, it.id)
             }
             startActivity(intent)
-        }
-        adapter.quandoSeguradoNoItem = {
-            val intent = Intent(
-                this,
-                FormularioProdutoActivity::class.java
-            ).apply {
-                putExtra(CHAVE_PRODUTO_ID, it.id)
-            }
-            val popupMenu: PopupMenu = PopupMenu(this, recyclerView)
-            popupMenu.menuInflater.inflate(R.menu.menu_popup_lista_produtos, popupMenu.menu)
-            popupMenu.setOnMenuItemClickListener { item ->
-                when (item.itemId) {
-                    R.id.menu_popup_lista_produtos_editar ->
-                        startActivity(intent)
-                    R.id.menu_popup_lista_produtos_remover -> {
-                        produtoDao.remove(it)
-                        adapter.atualiza(produtoDao.buscaTodos())
-                    }
-                }
-                true
-            }
-            popupMenu.show()
         }
     }
 
